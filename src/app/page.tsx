a1ff814737cce5bda9346070a7ff86f4be931851
Boris's PC <mircealike2013@gmail.com>
2024-04-15 15:59:14 +0300

    Finished description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,19 @@ export default function Home() {
       <Toolbar page="home"/>
       <div className="flex flex-col items-center justify-center w-screen h-96">
         <img src="/programing2.webp" alt="programing" className="absolute z-0 w-screen h-3/4 py-5 px-12 opacity-70 blur-md"/>
-        <div className="absolute flex flex-col justify-start items-center w-screen h-4/6"> 
-          <h1 className="z-10 text-4xl text-white pt-10">Welcome to Mircea's <span className="text-red-600 text-4xl font-bold p-1">Portfolio</span> </h1>
-          <p className="z-10 text-2xl text-gray-100 p-12"> Hello my friends</p>
+        <div className="absolute flex flex-col justify-center items-center w-4/6 h-4/6"> 
+          <h1 className="flex flex-row z-10 text-2xl text-white pt-10 xl:text-6xl">Welcome to Mircea's
+              <span className="text-red-600 text-2xl font-bold p-1 xl:text-6xl">Portfolio</span> 
+          </h1>
+          <p className="z-10 text-md text-gray-100 p-12 xl:text-4xl"> Hello, I am Mircea, a full-stack developer driven by a fervent passion for crafting clean, 
+              comprehensive, efficient, and innovative coding solutions. With adept proficiency in both backend and frontend technologies, I am committed to 
+              developing dynamic and holistic web applications.
+          </p>
+          <p className="z-10 text-md text-gray-100 p-12 xl:text-4xl">
+            My unwavering dedication to producing clean and maintainable code, meticulously centered around elevating the user experience, positions me 
+            to deliver seamless digital interactions. I invite you to explore my portfolio, a testament to my journey of mastering new technologies and evolving 
+            into the proficient web developer I am today.
+          </p>
         </div>
       </div>
       <Footer/>
